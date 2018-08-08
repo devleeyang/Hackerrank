b34a18ed2dff805e560a88ae526d77d0895b878f
yanghyeri <hr.yang@quintet.co.kr>
2018-08-08 11:56:54 +0900

    solved day0
    입력받은 값 출력하는 문제

---
+++ Day0/HelloWorld.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+let inputString = "Welcome to 30 Days of Code!" // get a line of input from stdin and save it to our variable
+
+// Your first line of output goes here
+print("Hello, World.")
+
+// Write the second line of output
+print(inputString)
